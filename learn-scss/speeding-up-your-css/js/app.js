a89fbb248a83bf2c32d7a50f6b7597082356e9b9
Trujun Zhang <trujunzhang@gmail.com>
2016-01-15 17:24:36 +0800

    speeding up your css with sass.

--- learn-scss/speeding-up-your-css/js/app.js
+++ learn-scss/speeding-up-your-css/js/app.js
@@ -1,3 +1,11 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(window).load(Function(){
+  $('.gallery').masonry({
+    itemSelector: '.gcontainer',
+    gutter: 15,
+    columnWidth:230
+  });
+});
