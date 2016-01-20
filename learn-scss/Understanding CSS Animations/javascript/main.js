a5c6f25aa40b1e54a15d130f228932326edfdc12
Trujun Zhang <trujunzhang@gmail.com>
2016-01-20 08:42:42 +0800

    Understanding CSS Animations.

---
+++ learn-scss/Understanding CSS Animations/javascript/main.js
@@ -0,0 +1,40 @@
+$(function () {
+    //var nav = $('.dropdown nav');
+    //nav.css('max-height','999px').hide();
+    //
+    //var dropdown = $('.dropdown');
+    //dropdown.click(function(){
+    //    var find = $(this).find('nav');
+    //    find.toggle();
+    //});
+
+    // jQuery plug-in
+    // var wh = $(window);
+    // wh.scroll(function () {
+    //     console.log($(window).scrollTop());
+    //     var scrollTop = $(window).scrollTop();
+    //     if (scrollTop > 1) {
+    //         var h1 = 'h1';
+    //         $(h1).removeClass('offToLeft').addClass('bounceInLeft');
+    //     }
+    // });
+
+
+    // Overwriting defaults
+    // var svg = new Walkway({
+    //     selector: 'svg',
+    //     duration: '2000',
+    //     // can pass in a function or a string like 'easeOutQuint'
+    //     easing: function (t) {
+    //         return t * t;
+    //     }
+    // });
+
+    // svg.draw();
+
+    // new WOW().init();
+
+    
+    $('.main').onepage_scroll();
+
+});
