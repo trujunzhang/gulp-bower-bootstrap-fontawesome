a9c277e463fe905d7510d3b8050448930eca69cb
Trujun Zhang <trujunzhang@gmail.com>
2016-03-07 19:23:56 +0800

    Up_and_Running_with_AngularJS.

---
+++ learn-scss/Up_and_Running_with_AngularJS/js/controller.js
@@ -0,0 +1,37 @@
+var artistControllers = angular.module('artistControllers', []);
+
+artistControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
+    $http.get('js/data.json').success(function (data) {
+        $scope.artists = data;
+        $scope.artistOrder = 'name'
+    });
+}]);
+
+
+artistControllers.controller('DetailsController',['$scope','$http','$routeParams', function($scope,$http,$routeParams){
+    var itemId = $routeParams.itemId;
+    $http.get('js/data.json').success(function(data){
+        $scope.artists = data;
+        $scope.whichItem = itemId;
+        $scope.item = data[itemId];
+
+        if(itemId > 0){
+            $scope.prevItem = Number(itemId) - 1;
+        }else{
+            $scope.prevItem = $scope.artists.length - 1;
+        }
+
+        if(itemId < $scope.artists.length - 1){
+            $scope.nextItem = Number(itemId) + 1;
+        }else{
+            $scope.nextItem = 0;
+        }
+        
+    })
+}]);
+
+
+
+
+
+
