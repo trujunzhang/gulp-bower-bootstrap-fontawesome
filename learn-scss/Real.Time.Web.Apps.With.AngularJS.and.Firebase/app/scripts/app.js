0e34917eb766b687af92e2f00707bdc3ee6046eb
Trujun Zhang <trujunzhang@gmail.com>
2016-03-10 17:54:58 +0800

    Login Prep.

--- learn-scss/Real.Time.Web.Apps.With.AngularJS.and.Firebase/app/scripts/app.js
+++ learn-scss/Real.Time.Web.Apps.With.AngularJS.and.Firebase/app/scripts/app.js
@@ -31,6 +31,10 @@ angular
         templateUrl: 'views/login.html',
         controller: 'LoginCtrl'
       })
+      .when('/logout',{
+        template: 'Logging out ...',
+        controller: 'LogoutCtrl'
+      })
       .when('/register', {
         templateUrl: 'views/register.html',
         controller: 'RegisterCtrl',
